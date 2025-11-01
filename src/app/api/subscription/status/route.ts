a4392b2929c4b1702692ecc9553db1a175a43ecc
edmju <104655241+edmju@users.noreply.github.com>
2025-11-01 17:28:19 +0100

    Update route.ts

--- src/app/api/subscription/status/route.ts
+++ src/app/api/subscription/status/route.ts
@@ -14,6 +14,7 @@ export async function GET() {
     const user = await prisma.user.findUnique({
       where: { email: session.user.email },
     });
+
     if (!user) {
       return NextResponse.json({ active: false }, { status: 200 });
     }
@@ -27,7 +28,6 @@ export async function GET() {
     if (!sub) {
       return NextResponse.json({ active: false }, { status: 200 });
     }
-
     return NextResponse.json(
       {
         active: true,

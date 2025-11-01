b602bf350dac47eecc2003d3c5d326e089748a53
edmju <104655241+edmju@users.noreply.github.com>
2025-11-01 17:47:24 +0100

    Update route.ts

--- src/app/api/stripe/portal/route.ts
+++ src/app/api/stripe/portal/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {

07d11dcbca28ea0be56f35c90253611f9a434a74
edmju <104655241+edmju@users.noreply.github.com>
2025-11-05 23:35:01 +0100

    Create page.tsx

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/site/Hero";
+import FeatureGrid from "@/components/site/FeatureGrid";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeatureGrid />
+    </>
+  );
+}
